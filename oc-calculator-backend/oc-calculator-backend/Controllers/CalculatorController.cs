0c86d2030521fff1c90845fdbe4eb97d127ff8b3
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2026-03-15 11:46:53 +0900

    feat: Created endpoint to receive values and operator, that calls the respective method for the operator received

---
+++ oc-calculator-backend/oc-calculator-backend/Controllers/CalculatorController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using oc_calculator_backend.Models;
+
+namespace oc_calculator_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpPost("calculate")]
+        public IActionResult Calculate(int a, int b, string operation)
+        {
+            var result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = Sum(a, b);
+                    break;
+                case "-":
+                    result = Subtract(a, b);
+                    break;
+                case "*":
+                    result = Multiply(a, b);
+                    break;
+                case "/":
+                    result = Divide(a, b);
+                    break;
+                default:
+                    //throw new NotImplementedException();
+                    return BadRequest("Invalid operation");
+            }
+
+            return Ok(result);
+        }
+
+        private int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        private int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        private int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ oc-calculator-backend/oc-calculator-backend/Models/BasicOperationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace oc_calculator_backend.Models
+{
+    public class BasicOperationModel
+    {
+        public int valueA { get; set; }
+        public int valueB { get; set; }
+        public int result { get; set; }
+    }
+}
